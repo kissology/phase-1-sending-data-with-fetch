c5f9780555bb58bb761b65ae40815534a25df550
Caitlin Ma <109817775+kissology@users.noreply.github.com>
2023-10-18 18:29:52 -0400

    commit tree d99be6cdf8c48c3e384fd12a310be95686690968

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(name, email){
+    const formData = {name: name,
+    email: email,};
+
+    const configurationData = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationData)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(object){
+        return object.id;
+    });
+}
